52cbe5d9b1139b492cee7ec04439d873126ea7ce
nekolr <excalibll@163.com>
2018-09-10 22:33:49 +0800

    Add matcher

--- ssm-starter-upms/ssm-starter-upms-server/src/main/java/com/nekolr/upms/server/shiro/realm/RealmManager.java
+++ ssm-starter-upms/ssm-starter-upms-server/src/main/java/com/nekolr/upms/server/shiro/realm/RealmManager.java
@@ -1,6 +1,10 @@
 package com.nekolr.upms.server.shiro.realm;
 
+import com.nekolr.upms.api.rpc.UserService;
+import com.nekolr.upms.server.shiro.matcher.JwtMatcher;
+import com.nekolr.upms.server.shiro.matcher.PasswordMatcher;
 import org.apache.shiro.realm.Realm;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -12,6 +16,13 @@ import java.util.Collection;
  */
 @Component
 public class RealmManager {
+
+    @Autowired
+    private PasswordMatcher passwordMatcher;
+    @Autowired
+    private JwtMatcher jwtMatcher;
+    @Autowired
+    private UserService userService;
 
     public Collection<Realm> initRealms() {
         return null;
